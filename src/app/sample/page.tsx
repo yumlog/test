c2c9bf02bc3e658f3b3e7f1bce76c51fdae2ec78
yumlog <yumlog314@gmail.com>
2025-04-08 15:21:01 +0900

    tailwind 커스텀

---
+++ src/app/sample/page.tsx
@@ -0,0 +1,8 @@
+export default function Sample() {
+  return (
+    <div className="flex flex-col gap-y-5 bg-gray-6 rounded-xs">
+      <p className="text-redd text-h1">빨간색</p>
+      <p className="text-bluee">파란색</p>
+    </div>
+  )
+}
